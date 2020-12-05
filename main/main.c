89d0d72e7d89d24b07790bd0b1ac3b0efa77e273
Nacht <nacht42@riseup.net>
2020-12-05 23:59:38 +0800

    feat(main): print "hello, world!" on lcd

--- main/main.c
+++ main/main.c
@@ -1,14 +1,16 @@
 #include <stdio.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+#include "ssd1306.h"
 
+#define SDA_PIN CONFIG_SDA_GPIO_PIN
+#define SCL_PIN CONFIG_SCL_GPIO_PIN
 
 void app_main(void)
 {
-    int i = 0;
-    while (1) {
-        printf("[%d] Hello world!\n", i);
-        i++;
-        vTaskDelay(5000 / portTICK_PERIOD_MS);
-    }
+
+    ssd1306_128x64_i2c_initEx(SCL_PIN, SDA_PIN, 0); // 0 means default i2c address of lcd display
+    ssd1306_clearScreen();
+    ssd1306_setFixedFont(ssd1306xled_font6x8);
+    ssd1306_printFixed(0, 8, "hello, world!", STYLE_NORMAL);
 }
